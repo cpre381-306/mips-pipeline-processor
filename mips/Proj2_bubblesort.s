eda135c7d1cbfdb95b12894f49bde9a882ae1261
edknott <edknott@vlinux-18.ece.iastate.edu>
2022-04-29 10:09:59 -0500

    t bubblesort toba able to updat

--- mips/Proj2_bubblesort.s
+++ mips/Proj2_bubblesort.s
@@ -10,40 +10,39 @@ main:
 	li $t1, 8		# get size of array
 
 loop_main:
-	subi $a1, $t1, 1	# count for current pass
+	subi $a1, $t1, 1	# depends on t1 after initial run (PROBLEM)
 
-	ble $a1, $0, end	# check if complete
+	ble $a1, $0, end	# depends on subi 
 
-	la $a0, Arr		# get array address
-	li $t2, 0		# clear swap flag
-
-	jal loop		# single sort pass
-
-	beq $t2, $0, end	# if no swaps then sort = done
-
-	subi $t1, $t1, 1	# decrease remaining pass count
-
-	j loop_main		# go back to start of loop
+	la $a0, Arr		# depends on a0 (no worry)
+	li $t2, 0		# depends on t2 (no worry)
+	jal loop		# set ra (no issue)
+	beq $t2, $0, end	# depends on t2(no worry)
+	subi $t1, $t1, 1	# depends on t1(no worry)
+	j loop_main		# (no issue)
+V include 2 stalls
 
 loop:
-	lw $s1, 0($a0)		# loads first element in $s1
-	lw $s2, 4($a0)		# loads second element in $s2
-
-	bgt $s1, $s2, swap	# if s2 < s1 then swap
+^include one stall
+	lw $s1, 0($a0)		# depends on a0(2 blocks away) wait for JAL(?)(PROBLEM)
+^if stall included, no issue should arise	
+	lw $s2, 4($a0)		# depends on a0 (JAL?)(PROBLEM)
+^need 3(?) stalls
+	bgt $s1, $s2, swap	# depends on s1 and s2(PROBLEM)
 
 next:
-	addiu $a0, $a0, 4	# next element
+	addiu $a0, $a0, 4	# depends on a0 (no worry)
+	subiu $a1, $a1, 1	# depends on a1(no worry)
+^need 3(?) stalls
+	bgt $a1, $0, loop	# dpends on a1 (PROBLEM)
 
-	subiu $a1, $a1, 1	# decrease number of loops
-	bgt $a1, $0, loop	# checks for swap or goes to loop
-
-	jr $ra			# returns
+	jr $ra			# go to jal(no issue)
 
 swap:
-	sw $s1, 4($a0)		# puts value of [i+1] in s1
-	sw $s2, 0($a0)		# puts value of [i] in s2
-	li $t2, 1		# notes a swap
-	j next			# jump to next
+	sw $s1, 4($a0)		# depends on a0(no issue)
+	sw $s2, 0($a0)		# depends on a0(no issue)
+	li $t2, 1		# depends on t2(no issue)
+	j next			# (no issue)
 
 end:
 	li $v0, 10
